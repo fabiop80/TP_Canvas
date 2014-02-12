f5c3e4c84339386a5f50cc989686b2f9ad31bcce
smav12 <smavbg@gmail.com>
2014-02-12 07:02:14 -0500

    le cadre

--- scripts.js
+++ scripts.js
@@ -8,6 +8,8 @@ var tabFiltre;
 
 
 window.onload = function(){
+
+  
 	if(localStorage.getItem("tabPhotos")!=null && localStorage.getItem("tabFiltres")!=null)	{
 	  tabPhotos = JSON.parse(localStorage.getItem("tabPhotos"));
 	  tabFiltre = JSON.parse(localStorage.getItem("tabFiltres"));
@@ -16,6 +18,10 @@ window.onload = function(){
 	 tabFiltre = new Array();
 	}
 
+	$('#idFile').bind("click" , function () {
+        $('#html_btn').click();
+    });
+	
 	var image = new Image();
 
 	defaultValues();
@@ -29,17 +35,13 @@ window.onload = function(){
 	btnSauvegardes.addEventListener('click',sauvegarder, false)
 	
 	document.getElementById("idReset").onclick = reset;
-//    image.src = localStorage.getItem("src");
-	
-	image.onload = function(){
-	canvas.width = image.width;
-	canvas.height = image.height;
-		ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
-	}
+	document.getElementById("html_btn").onchange = loadFile;
+
 	
 	if (tabPhotos.length > 0 && tabFiltre.length > 0){
 		afficherPhoto(tabPhotos, tabFiltre);
 	}
+	
 	image.onload=function(){
 		this.width = canvas.width;
 		this.height = canvas.height;
@@ -75,3 +77,32 @@ window.onload = function(){
 			}
 	
      }
+	 
+function cadre(x,y){
+ //var l1 = x*1.26436782;
+ var l1 = x*1.22324159;
+ //var h1 = y*1.36363636;
+ var h1 = y*1.325;
+ var monImage= document.getElementById("cadre");
+ monImage.width = l1;
+ monImage.height = h1;
+
+}	 
+	 
+function loadFile(){
+	var monFichier = document.getElementsByTagName("input")[0].files[0];
+	var urlObj = window.URL.createObjectURL(monFichier);
+	var image = new Image();
+	image.src = urlObj;
+	
+	image.onload = function(){
+		var cwdt = canvas.width;
+		var twdt = this.width;
+		var thdt = this.height;		
+		canvas.height = (cwdt/twdt)*thdt;
+		//cadre(canvas.width,canvas.height);
+		ctx.drawImage(image, 10,10, canvas.width, canvas.height);	
+        ctx.drawImage(document.getElementById("cadre"), 0, 0, canvas.width, canvas.height);			
+		window.URL.revokeObjectURL(urlObj);
+	}						    
+}

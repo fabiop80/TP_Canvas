3c5116c5ff919000332aefa12eff394b11c3395a
smav12 <smavbg@gmail.com>
2014-02-12 15:02:26 -0500

    last change about pick a color

--- scripts.js
+++ scripts.js
@@ -5,11 +5,11 @@ var ctx;
 var video;
 var tabPhotos;
 var tabFiltre;
-
+var imageDataBkp;
 
 window.onload = function(){
 
-  
+   
 	if(localStorage.getItem("tabPhotos")!=null && localStorage.getItem("tabFiltres")!=null)	{
 	  tabPhotos = JSON.parse(localStorage.getItem("tabPhotos"));
 	  tabFiltre = JSON.parse(localStorage.getItem("tabFiltres"));
@@ -42,11 +42,12 @@ window.onload = function(){
 		afficherPhoto(tabPhotos, tabFiltre);
 	}
 	
-	image.onload=function(){
+	/*image.onload=function(){
 		this.width = canvas.width;
 		this.height = canvas.height;
 		ctx.drawImage(this,0,0,canvas.width,canvas.height);
-	}
+		imageDataBkp = ctx.getImageData(0, 0, canvas.width, canvas.height);	
+	}*/
 //	canvas.style.webkitFilter=localStorage.getItem("filtres");	
 
 	video = document.getElementById("my_video");
@@ -72,23 +73,13 @@ window.onload = function(){
 				canvas.width = video.videoWidth;
 				canvas.height = video.videoHeight;
 				ctx.drawImage(video, 0, 0, canvas.width, canvas.height); /* Je dessine sur mon canvas ce que mon image film*/ 
+				imageDataBkp = ctx.getImageData(0,0,canvas.width,canvas.height);	
 				reset();
 			
 			}
 	
      }
-	 
-function cadre(x,y){
- //var l1 = x*1.26436782;
- var l1 = x*1.22324159;
- //var h1 = y*1.36363636;
- var h1 = y*1.325;
- var monImage= document.getElementById("cadre");
- monImage.width = l1;
- monImage.height = h1;
-
-}	 
-	 
+ 
 function loadFile(){
 	var monFichier = document.getElementsByTagName("input")[0].files[0];
 	var urlObj = window.URL.createObjectURL(monFichier);
@@ -98,11 +89,33 @@ function loadFile(){
 	image.onload = function(){
 		var cwdt = canvas.width;
 		var twdt = this.width;
-		var thdt = this.height;		
-		canvas.height = (cwdt/twdt)*thdt;
-		//cadre(canvas.width,canvas.height);
-		ctx.drawImage(image, 10,10, canvas.width, canvas.height);	
-        ctx.drawImage(document.getElementById("cadre"), 0, 0, canvas.width, canvas.height);			
+		var thdt = this.height;	
+        var leCadre = document.getElementById("cadre");		
+		ctx.drawImage(image, 0,0, canvas.width, canvas.height);	
+		imageDataBkp = ctx.getImageData(0,0,canvas.width, canvas.height);	
+        			
 		window.URL.revokeObjectURL(urlObj);
 	}						    
+}
+
+//Event for reset button	 
+ function reset(){
+	canvas.removeAttribute("style");
+	for(var i=0; i< arrayDefault.length; i++){
+		document.getElementsByTagName('input')[i+1].value = arrayDefault[i];
+	}
+	defaultValues();
+	ctx.putImageData(imageDataBkp,0,0);
+}
+//Function to put all values on default values	
+function defaultValues(){
+bluValue=0;
+hueRValue=0;
+invtValue=0;
+brightnValue=1;
+sepiValue=0;
+graysaValue=0;
+opaciValue=1;
+satuvalue=1;
+contrstValue=1;
 }
